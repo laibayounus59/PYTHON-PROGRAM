d877dda920031342b906837c32cb177d157ef5ca
laibayounus59 <laibayounus27@gmail.com>
2024-06-12 00:08:33 +0500

    Add files via upload

---
+++ laibayounus.py
@@ -0,0 +1,14 @@
+#FOR AREA OF TRIANGLE
+base=7.7
+height=3.4
+Area_of_triangle=0.5*base*height
+print("The area of triangle is:",Area_of_triangle)
+#FOR AREA OF A SQUARE
+side=6
+area_of_square=side*side
+print("Area of square is:",area_of_square)
+#FOR AREA OF CIRCLE
+radius=7
+pi=3.142
+area_of_circle =pi*radius*radius
+print("Area of circle:",area_of_circle)
